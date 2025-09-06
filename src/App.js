54f0e5eed2beebc02e95d0cfd4947a713c872008
visheshtanwar27 <potssteve187@gmail.com>
2025-09-06 19:14:38 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import {BrowserRouter , Route , Routes} from "react-router-dom" ;
+import Register from "./components/register.js";
+import Login from "./components/login.js";
+import Profile from "./components/profile.js";
+import FileComplaint from "./components/fileComplaint.js"; 
+import ComplaintDetails from "./components/complaintDetails.js";
+import AdminLogin from "./components/adminLogin.js";
+import Dashboard from "./components/adminDashboard.js";
+import UpdateGrievance from "./components/updateGrievance.js";
+import ActionDetails from "./components/actionDetails.js";
+import Navbar from "./components/navbar.js";
+import Home from "./components/home.js";
+import Admins from "./components/admins.js";
+import AdminRegister from "./components/adminRegister.js"; 
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/> 
+      <Routes>
+          <Route path="/" element={<Home/>}/>  
+          <Route path="/newRegistration" element={<Register/>}/>  
+          <Route path="/login" element={<Login/>}/>  
+          <Route path="/profile" element={<Profile/>}/>  
+          <Route path="/fileComplaint" element={<FileComplaint/>}/>
+          <Route path="/complaintDetails" element={<ComplaintDetails/>}/> 
+          <Route path="/adminLogin" element={<AdminLogin/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/> 
+          <Route path="/updateGrievance/:grievanceId" element={<UpdateGrievance/>}/>
+          <Route path="/actionDetails" element={<ActionDetails/>} /> 
+          <Route path="/allAdmins" element={<Admins/>}/> 
+          <Route path="/cvh-admin" element={<AdminRegister/>}/>  
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/components/adminDashboard.js
@@ -0,0 +1,175 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./dashboard.css";
+
+const Dashboard = () => {
+  const [complaints, setComplaints] = useState([]);
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [status, setStatus] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          // "http://localhost:8000/admin/allGrievance",
+          'https://project-y58m.onrender.com/admin/allGrievance',  
+          {
+            withCredentials: true,
+          }
+        );
+        if (response.status === 200) {
+          setComplaints(response.data);
+        }
+      } catch (e) {
+        console.error("Error fetching complaints:", e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleDelete = (grievanceId) => async () => {
+    if (!window.confirm("Are you sure you want to delete this grievance?")) {
+      return;
+    }
+    try {
+      const response = await axios.delete(
+        // `http://localhost:8000/admin/deleteGrievance/${grievanceId}`,
+        `https://project-y58m.onrender.com/admin/deleteGrievance/${grievanceId}`, 
+        { withCredentials: true }
+      );
+      if (response.status === 200) {
+        setComplaints((prevState) =>
+          prevState.filter((complaint) => complaint._id !== grievanceId)
+        );
+        console.log("deleted successfully");
+      }
+    } catch (e) {
+      console.error("error deleting grievance ", e);
+    }
+  };
+
+  const handleStartDate = (e) => {
+    const val = e.target.value;
+    setStartDate(val);
+  };
+
+  const handleEndDate = (e) => {
+    const val = e.target.value;
+    setEndDate(val);
+  };
+
+  const handleFilter = async () => {
+    const response = await axios.post(
+      "https://project-y58m.onrender.com/admin/filterGrievance",
+      { startDate, endDate },
+      { withCredentials: true }
+    );
+    setComplaints(response.data);
+  };
+
+  const handleRemoveFilter = async () => {
+    const response = await axios.get(
+      "https://project-y58m.onrender.com/admin/allGrievance",
+      { withCredentials: true }
+    );
+    setComplaints(response.data);
+  };
+
+  const handleStatus = (e) => {
+    setStatus(e.target.value); 
+  };  
+
+  const handleStatusFilter = async() => {
+    console.log(status); 
+    const response = await axios.post('https://project-y58m.onrender.com/admin/statusFilter',{status},{withCredentials:true}) ;
+    setComplaints(response.data) ; 
+  };
+
+  // Function to get the status color based on value
+  const getStatusColor = (status) => {
+    return status === "Seen" ? "green" : "red";
+  };
+
+  return (
+    <div className="dashboard-container">
+      <div className="filter-section">
+        <h3>Filter by Date</h3>
+        <input
+          type="date"
+          placeholder="Start Date"
+          value={startDate}
+          onChange={handleStartDate}
+        />
+        <input
+          type="date"
+          placeholder="End Date"
+          value={endDate}
+          onChange={handleEndDate}
+        />
+        <button onClick={handleFilter}>Apply Filter</button>
+        <button onClick={handleRemoveFilter}>Remove Filter</button>
+      </div>
+      
+      <div className="filter-section">
+        <h3>Filter by Status</h3>
+        <select value={status} onChange={handleStatus}>
+          <option value="Seen">Seen</option>
+          <option value="Not Seen">Not Seen</option>
+        </select>
+        <button onClick={handleStatusFilter}>Apply</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>GrievanceID</th>
+            <th>Grievance</th>
+            <th>UserID</th>
+            <th>User Name</th>
+            <th>Status</th>
+            <th>Action</th>
+            <th>Date</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {complaints.map((data, idx) => (
+            <tr key={idx}>
+              <td>{data._id}</td>
+              <td>{data.grievance}</td>
+              <td>{data.user._id}</td>
+              <td>{data.user.name}</td>
+              <td style={{ color: getStatusColor(data.status) }}>
+                {data.status}
+              </td>
+              <td>{data.action}</td>
+              <td>{new Date(data.date).toLocaleDateString()}</td>
+              <td>
+                <button
+                  onClick={() => navigate(`/updateGrievance/${data._id}`)}
+                >
+                  Update
+                </button>
+              </td>
+              <td>
+                <button 
+                  className="delete" 
+                  onClick={handleDelete(data._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Dashboard;
